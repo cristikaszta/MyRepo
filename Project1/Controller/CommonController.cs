f130a1a34adf2026eb40da675a645cfd5a5681a2
Cristian K <cristikaszta@gmail.com>
2016-04-28 01:55:17 +0300

    Add CommonController

---
+++ Project1/Controller/CommonController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace DisertationProject.Controller
+{
+    public class CommonController
+    {
+        /// <summary>
+        /// Method used to add items to dictionaries
+        /// </summary>
+        /// <typeparam name="T1">Dictionary key type parameter</typeparam>
+        /// <typeparam name="T2">Dictionary value type parameter</typeparam>
+        /// <param name="dictionary">The dictionary</param>
+        /// <param name="key">The key</param>
+        /// <param name="value">The value</param>
+        public void addItemToDictionary<T1, T2>(IDictionary<T1, T2> dictionary, T1 key, Func<T1, T2> value)
+        {
+            dictionary.Add(key, (T2)value(key));
+        }
+
+        /// <summary>
+        /// Method used to add items to dictionaries
+        /// </summary>
+        /// <typeparam name="T1">Dictionary key type parameter</typeparam>
+        /// <typeparam name="T2">Dictionary value type parameter</typeparam>
+        /// <param name="dictionary">The dictionary</param>
+        /// <param name="key">The key</param>
+        /// <param name="value">The value</param>
+        public void addItemToDictionary<T1, T2>(IDictionary<T1, T2> dictionary, T1 key, T2 value)
+        {
+            dictionary.Add(key, value);
+        }
+    }
+}
